b618cd818bf8a712b346154302650d05b1c0c99e
JaxonZaleski <zaleskjn@mail.uc.edu>
2024-09-21 13:40:43 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double winterCost = 0.00;
+        double springCost = 0.00;
+        double summerCost = 0.00;
+        double fallCost = 0.00;
+        double totalCost = 0.00;
+
+        totalCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println ("The winter maintenance cost is " + winterCost);
+        System.out.println ("The spring maintenance cost is " + springCost);
+        System.out.println ("The summer maintenance cost is " + summerCost);
+        System.out.println ("The fall maintenance cost is " + fallCost);
+        System.out.println ("The total maintenance cost is " + totalCost);
+    }
+}
